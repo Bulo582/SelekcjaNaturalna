f8e135e72f63ed5990e97c6d92b468bec618c6cc
Tomasz Radomski <39155653+Bulo582@users.noreply.github.com>
2020-03-05 11:43:44 +0100

    Movement 0.4
    Direction move system
    ChooseWay()

---
+++ Assets/RotTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.transform.rotation = Quaternion.Euler(0, -90, 0);
+        this.gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+    }
+
+
+
+}
--- Assets/Scripts/Movement/Movement.cs
+++ Assets/Scripts/Movement/Movement.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 
 public enum Direction
 {
-    forward,
+    up,
     right,
     down,
-    left
+    left,
+    none
 }
-
 
 public class Movement : MonoBehaviour
 {
-    Direction direction = Direction.right;
     float animationTime = 2f;
     float globalMovementIncrease = 0.5f;
     public float movementSpeed;
@@ -28,6 +27,10 @@ public class Movement : MonoBehaviour
 
     int halfWidthMap;
     int halfHeightMap;
+
+    DirMove dirMove;
+
+    private char[] blockableChar = { 'X', 'T', 'C', 'R'};
     void Start()
     {
         actualPosition = this.gameObject.transform.position;
@@ -40,11 +43,16 @@ public class Movement : MonoBehaviour
         arrayPozX = Convert.ToInt16(actualPosition.x ) + halfHeightMap;
         arrayPozZ = Convert.ToInt16(actualPosition.z) - halfWidthMap;
 
-        accesArea = ArrayModify.CircleOut(Spawner.Instance.GenerateMap, arrayPozZ, arrayPozX, 2); // reverse argument x/z
+        accesArea = ArrayModify.CircleOut(Spawner.Instance.GenerateMap, arrayPozZ, arrayPozX, 2); //tip! reverse argument x/z
 
+        dirMove = new DirMove(Direction.right);
+    }
+
+    public void OldLog()
+    {
         foreach (var item in accesArea)
         {
-           Debug.Log(item.ToString());
+            Debug.Log(item.ToString());
         }
         Debug.Log($"X = {arrayPozX} Z = {arrayPozZ}");
         Debug.Log($"What is on postion = {ArrayModify.TypeField(Spawner.Instance.GenerateMap, arrayPozX, arrayPozZ)}");
@@ -61,34 +69,282 @@ public class Movement : MonoBehaviour
             StartCoroutine("MoveTime", animationTime);
         }
     }
-
     public IEnumerator MoveTime()
     {
         movementCooldown = 0;
         animationWork = true;
-        //RandomMove();
+        RandomMove();
         yield return new WaitForSeconds(animationTime);
         Debug.Log("Jump");
         animationWork = false;
     }
-
     public void RandomMove()
     {
-        if(direction == Direction.right)
+        ChooseWay(accesArea);
+        DirToArrayPoz();
+    }
+    /// <summary>
+    /// Atrer take way 
+    /// Change: arrayPozX, arrayPozZ, Spawner.Instance.GenerateMap[x,y]
+    /// </summary>
+    public void DirToArrayPoz()
+    {
+        if (dirMove.dir == Direction.up)
         {
-            this.gameObject.transform.Translate(0, 0, 1);
+            Spawner.Instance.GenerateMap[arrayPozX, arrayPozZ] = '4';
+            arrayPozX++;
+            Spawner.Instance.GenerateMap[arrayPozX, arrayPozZ] = 'R';
         }
-        else if(direction == Direction.down)
+        else if (dirMove.dir == Direction.down)
         {
+            Spawner.Instance.GenerateMap[arrayPozX, arrayPozZ] = '4';
+            arrayPozX--;
+            Spawner.Instance.GenerateMap[arrayPozX, arrayPozZ] = 'R';
+        }
+        else if (dirMove.dir == Direction.right)
+        {
+            Spawner.Instance.GenerateMap[arrayPozX, arrayPozZ] = '4';
+            arrayPozZ++;
+            Spawner.Instance.GenerateMap[arrayPozX, arrayPozZ] = 'R';
+        }
+        else if (dirMove.dir == Direction.left)
+        {
+            Spawner.Instance.GenerateMap[arrayPozX, arrayPozZ] = '4';
+            arrayPozZ--;
+            Spawner.Instance.GenerateMap[arrayPozX, arrayPozZ] = 'R';
+        }
+    }
+    public static Vector3 DirToVect3(Direction direction)
+    {
+        if (direction == Direction.up)
+            return new Vector3(-1, 0, 0);
+        else if (direction == Direction.left)
+            return new Vector3(0, 0, -1);
+        else if (direction == Direction.down)
+            return new Vector3(1, 0, 0);
+        else if (direction == Direction.right)
+            return new Vector3(0, 0, 1);
+        else return new Vector3(0, 0, 0);
+    }
+    int WayIndex(bool[] pos)
+    {
+        if (pos.Length != 3)
+            throw new Exception("tabela 'pos' musi być [3]");
+        int rnd;
+        int inx = 0;
+        while (inx < 10)
+        {
+            rnd = UnityEngine.Random.Range(0, 2);
+            if (pos[rnd] == true)
+                return rnd;
+            inx++;
+        }
+        return 3;
+    }
+    public void ChooseWay(char[,] accesArea)
+    {
+        if (dirMove.dir == Direction.right)
+        {
+            if (Array.Exists(blockableChar, x => x == accesArea[0, 1]))
+                dirMove.pos[0] = false;
+            else
+                dirMove.pos[0] = true;
+
+            if (Array.Exists(blockableChar, x => x == accesArea[1, 2]))
+                dirMove.pos[1] = false;
+            else
+                dirMove.pos[1] = true;
+
+            if (Array.Exists(blockableChar, x => x == accesArea[2, 1]))
+                dirMove.pos[2] = false;
+            else
+                dirMove.pos[2] = true;
+
+            int inx = WayIndex(dirMove.pos);
+
+            if (inx == 0)
+            {
+                dirMove.move = DirToVect3(Direction.down);
+                dirMove.dir = Direction.down;
+                TurnObject(dirMove.dir);
+            }
+            else if (inx == 1)
+            {
+                dirMove.move = DirToVect3(Direction.right);
+                dirMove.dir = Direction.right;
+            }
+            else if (inx == 2)
+            {
+                dirMove.move = DirToVect3(Direction.up);
+                dirMove.dir = Direction.up;
+                TurnObject(dirMove.dir);
+            }
+            else
+            {
+                dirMove.move = DirToVect3(Direction.left);
+                dirMove.dir = Direction.left;
+                TurnObject(dirMove.dir);
+            }
 
         }
-        else if(direction == Direction.forward)
+        else if (dirMove.dir == Direction.down)
         {
+            if (Array.Exists(blockableChar, x => x == accesArea[1, 0]))
+                dirMove.pos[0] = false;
+            else
+                dirMove.pos[0] = true;
+
+            if (Array.Exists(blockableChar, x => x == accesArea[2, 1]))
+                dirMove.pos[1] = false;
+            else
+                dirMove.pos[1] = true;
+
+            if (Array.Exists(blockableChar, x => x == accesArea[1, 2]))
+                dirMove.pos[2] = false;
+            else
+                dirMove.pos[2] = true;
+
+            int inx = WayIndex(dirMove.pos);
+
+            if (inx == 0)
+            {
+                dirMove.move = DirToVect3(Direction.left);
+                dirMove.dir = Direction.left;
+                TurnObject(dirMove.dir);
+            }
+            else if (inx == 1)
+            {
+                dirMove.move = DirToVect3(Direction.down);
+                dirMove.dir = Direction.down;
+            }
+            else if (inx == 2)
+            {
+                dirMove.move = DirToVect3(Direction.right);
+                dirMove.dir = Direction.right;
+                TurnObject(dirMove.dir);
+            }
+            else
+            {
+                dirMove.move = DirToVect3(Direction.up);
+                dirMove.dir = Direction.up;
+                TurnObject(dirMove.dir);
+            }
+
 
         }
-        else if(direction == Direction. left)
+        else if (dirMove.dir == Direction.up)
         {
+            if (Array.Exists(blockableChar, x => x == accesArea[1, 0]))
+                dirMove.pos[0] = false;
+            else
+                dirMove.pos[0] = true;
+
+            if (Array.Exists(blockableChar, x => x == accesArea[1, 1]))
+                dirMove.pos[1] = false;
+            else
+                dirMove.pos[1] = true;
+
+            if (Array.Exists(blockableChar, x => x == accesArea[1, 2]))
+                dirMove.pos[2] = false;
+            else
+                dirMove.pos[2] = true;
+
+            int inx = WayIndex(dirMove.pos);
+
+            if (inx == 0)
+            {
+                dirMove.move = DirToVect3(Direction.left);
+                dirMove.dir = Direction.left;
+                TurnObject(dirMove.dir);
+            }
+            else if (inx == 1)
+            {
+                dirMove.move = DirToVect3(Direction.up);
+                dirMove.dir = Direction.up;
+            }
+            else if (inx == 2)
+            {
+                dirMove.move = DirToVect3(Direction.right);
+                dirMove.dir = Direction.right;
+                TurnObject(dirMove.dir);
+            }
+            else
+            {
+                dirMove.move = DirToVect3(Direction.down);
+                dirMove.dir = Direction.down;
+                TurnObject(dirMove.dir);
+            }
+
+        }
+        else if (dirMove.dir == Direction.left)
+        {
+            if (Array.Exists(blockableChar, x => x == accesArea[2, 1]))
+                dirMove.pos[0] = false;
+            else
+                dirMove.pos[0] = true;
+
+            if (Array.Exists(blockableChar, x => x == accesArea[1, 0]))
+                dirMove.pos[1] = false;
+            else
+                dirMove.pos[1] = true;
+
+            if (Array.Exists(blockableChar, x => x == accesArea[1, 1]))
+                dirMove.pos[2] = false;
+            else
+                dirMove.pos[2] = true;
+
+            int inx = WayIndex(dirMove.pos);
+
+            if (inx == 0)
+            {
+                dirMove.move = DirToVect3(Direction.down);
+                dirMove.dir = Direction.down;
+                TurnObject(dirMove.dir);
+            }
+            else if (inx == 1)
+            {
+                dirMove.move = DirToVect3(Direction.left);
+                dirMove.dir = Direction.left;
+            }
+            else if (inx == 2)
+            {
+                dirMove.move = DirToVect3(Direction.up);
+                dirMove.dir = Direction.up;
+                TurnObject(dirMove.dir);
+            }
+            else
+            {
+                dirMove.move = DirToVect3(Direction.right);
+                dirMove.dir = Direction.right;
+                TurnObject(dirMove.dir);
+            }
 
         }
     }
+    public void TurnObject(Direction dir)
+    {
+        if (dir == Direction.right)
+            this.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+        else if (dir == Direction.up)
+            this.gameObject.transform.rotation = Quaternion.Euler(0, -90, 0);
+        else if(dir == Direction.down)
+            this.gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+        else if (dir == Direction.left)
+            this.gameObject.transform.rotation = Quaternion.Euler(0, -180, 0);
+    }
+    public struct DirMove
+    {
+        public Direction dir;
+        public bool[] pos;
+        public Vector3 move;
+
+        public DirMove(Direction dir)
+        {
+            this.dir = dir;
+            this.pos = new bool[3];
+            move = Movement.DirToVect3(Direction.none);
+        }
+    }
 }
+
+
